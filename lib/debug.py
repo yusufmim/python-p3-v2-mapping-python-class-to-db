752f42116aa4a11996ce4012fbb7498d4219485a
yusufmim <yusufmim2001@gmail.com>
2025-05-29 20:52:17 +0300

    init commit

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,13 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+d1 = Department(name="Engineering", location="Building 1")
+print("Before save:", d1)
+
+d1.save()
+print("After save:", d1)
+
 ipdb.set_trace()
