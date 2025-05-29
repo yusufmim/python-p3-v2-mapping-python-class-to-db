172c791c70b658155a24716f51f1f54e62179967
yusufmim <yusufmim2001@gmail.com>
2025-05-29 20:48:04 +0300

    init commit

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,36 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+        
+        
+        @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = "DROP TABLE IF EXISTS departments;"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):  # ← This is #3
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):  # ← This is #4
+        department = cls(name, location)
+        department.save()
+        return department
