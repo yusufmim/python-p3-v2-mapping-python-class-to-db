9466f686fd576f28b12b97b7ffefd50ebe139857
yusufmim <yusufmim2001@gmail.com>
2025-06-01 19:55:30 +0300

    init commit

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,33 @@
-#!/usr/bin/env python3
 #lib/testing/debug.py
+#!/usr/bin/env python3
+
 
 from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
+
+# Reset table
+Department.drop_table()
+Department.create_table()
+
+# Create & save new department
+payroll = Department("Payroll", "Building A, 5th Floor")
+print("Before save:", payroll)
+payroll.save()
+print("After save:", payroll)
+
+# Create using class method
+hr = Department.create("Human Resources", "Building B, 2nd Floor")
+print("Created HR:", hr)
+
+# Update
+payroll.location = "Building C, 1st Floor"
+payroll.update()
+print("After update:", payroll)
+
+# Delete
+hr.delete()
+print("HR deleted")
 
 ipdb.set_trace()
--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,58 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+    
+
+    @classmethod
+    
+    def create_table(cls):
+        """Create the departments table if it doesn't exist"""
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the departments table if it exists"""
+        sql = "DROP TABLE IF EXISTS departments"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """Insert a new department into the database and assign the ID"""
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        """Create a new Department object and save it to the DB"""
+        dept = cls(name, location)
+        dept.save()
+        return dept
+
+    def update(self):
+        """Update an existing department in the DB"""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Delete this department from the database"""
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
